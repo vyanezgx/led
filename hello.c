ee704d5898aa0fc86071e6193054e9b6b495294e
por-aca <por.aca@gmail.com>
2017-06-17 12:15:38 -0500

    Definicion de Colores de LEDs

--- hello.c
+++ hello.c
@@ -1,34 +1,44 @@
 /*
-encendido secuencial de 3 leds usando makefile en raspberry
+* Encendido secuencial de 3 leds usando makefile en raspberry
+* 
+* Historial de cambios:
+* V1 - Original
+* v2 - Se agregaron #defines para los colores
+* 
 */
 
 #include<stdio.h>
 #include<pigpio.h>
 
+#define GREEN 17
+#define BLUE  27
+#define RED   22
+
+
 int main()
 {
-        if(!gpioInitialise()) return -1;
-        gpioSetMode(17, PI_OUTPUT);// green
-	gpioSetMode(27, PI_OUTPUT);// blue
-	gpioSetMode(22, PI_OUTPUT);// red
+	if(!gpioInitialise()) return -1;
+        gpioSetMode(GREEN, PI_OUTPUT);	// green
+		gpioSetMode(BLUE, PI_OUTPUT);	// blue
+		gpioSetMode(RED, PI_OUTPUT);	// red
 
 	
         while(1)
-        {       sleep(1);
-		gpioWrite(17, 0);
-	        gpioWrite(27, 1);
-	        gpioWrite(22, 1);               
- 		sleep(1);
+        {   sleep(1);
+			gpioWrite(GREEN, 0);
+	        gpioWrite(BLUE, 1);
+	        gpioWrite(RED, 1);               
+			sleep(1);
 
-                gpioWrite(17, 1);
-	        gpioWrite(27, 0);
-	        gpioWrite(22, 1);               
- 		sleep(1);        
+            gpioWrite(GREEN, 1);
+	        gpioWrite(BLUE, 0);
+	        gpioWrite(RED, 1);               
+			sleep(1);        
 
-                gpioWrite(17, 1);
-	        gpioWrite(27, 1);
-	        gpioWrite(22, 0);               
-                sleep(1);
+            gpioWrite(GREEN, 1);
+	        gpioWrite(BLUE, 1);
+	        gpioWrite(RED, 0);               
+            sleep(1);
         }
 return 0;
 }
